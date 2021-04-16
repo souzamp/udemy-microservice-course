764944730888a82c11699c657b042e548069e810
Marcos paulo <marcos.souza.anl@gmail.com>
2021-04-15 21:27:04 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/mircroservice/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/mircroservice/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,11 @@ package com.mircroservice.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+import jdk.jfr.Enabled;
+
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
